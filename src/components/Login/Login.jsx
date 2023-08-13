9d06ebd0319d8648c1db80ffd478f9b710ab1dc6
arpitsha25 <arpit.sha25@gmail.com>
2023-08-13 15:06:31 +0530

    commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
 const Login = () => {
+  const [logindata, setlogindata] = useState({ email: "", password: "" });
+  function handlelogin(){
+    console.log("handle login" , logindata)
+  }
   return (
-    <div>Login</div>
-  )
-}
+    <div className="container">
+      <div className="inner-container">
+        <div className="inner-container2">
+          <input 
+          type="email" 
+          placeholder="Email" 
+          name="email"
+          value={logindata.email}
+          onChange={(e)=>{setlogindata({...logindata , [e.target.name] : e.target.value})}}
+          />
 
-export default Login
+          <input 
+          type="password" 
+          placeholder="Password" 
+          name="password"
+          value={logindata.password}
+          onChange={(e)=>{setlogindata({...logindata, [e.target.name] : e.target.value})}}
+          />
+          <button className="btn" onClick={handlelogin}>Login</button>
+          <p>
+            Dont have account?
+            <Link to="/signup">Signup</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import "../Login/Login.css";
+import "./Signup.css";
+import { Link } from "react-router-dom";
 const Signup = () => {
+  const [signupdata, setsignupdata] = useState({
+    name: "",
+    contact: "",
+    email: "",
+    password: "",
+  });
+  function handlesignup() {
+    console.log("handle signup", signupdata);
+  }
   return (
-    <div>Sisgnup</div>
-  )
-}
+    <div className="container">
+      <div className="inner-container">
+        <div className="inner-container2">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={signupdata.name}
+            onChange={(e) => {
+              setsignupdata({ ...signupdata, [e.target.name]: e.target.value });
+            }}
+          />
 
-export default Signup
+          <input
+            type="number"
+            placeholder="Contact"
+            name="contact"
+            value={signupdata.contact}
+            onChange={(e) => {
+              setsignupdata({ ...signupdata, [e.target.name]: e.target.value });
+            }}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={signupdata.email}
+            onChange={(e) => {
+              setsignupdata({ ...signupdata, [e.target.name]: e.target.value });
+            }}
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={signupdata.password}
+            onChange={(e) => {
+              setsignupdata({ ...signupdata, [e.target.name]: e.target.value });
+            }}
+          />
+
+          <button className="btn" onClick={handlesignup}>Signup</button>
+          <p>
+            Already have an account?
+            <Link to="/">Login</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
